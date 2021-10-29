0c385f4f3dc4cee6f108b02f5d5177bacdca4108
unknown <mydocs3000@gmail.com>
2021-10-29 18:55:00 +0530

    added some style

--- frontend/src/places/pages/NewPlace.js
+++ frontend/src/places/pages/NewPlace.js
@@ -2,11 +2,11 @@ import React, { useState } from 'react';
 
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Button';
-import {
-  VALIDATOR_REQUIRE,
-  VALIDATOR_MINLENGTH
-} from '../../shared/util/validators';
-import { useForm } from '../../shared/hooks/form-hook';
+// import {
+//   VALIDATOR_REQUIRE,
+//   VALIDATOR_MINLENGTH
+// } from '../../shared/util/validators';
+// import { useForm } from '../../shared/hooks/form-hook';
 import './PlaceForm.css';
 
 const NewPlace = () => {
@@ -16,6 +16,7 @@ const NewPlace = () => {
     "location": ""
   })
   const handlechange = (e) => {
+    e.persist()
     setstate((prevstate) => {
       return { ...prevstate, [e.target.id]: e.target.value }
     })
@@ -45,43 +46,35 @@ const NewPlace = () => {
 
 
   return (
-    <form  onSubmit={placeSubmitHandler}>
+    <form onSubmit={placeSubmitHandler}>
       <div
-        className={`place-form`}
-      >
+        className={`place-form`}>
+        <label>Name</label>
         <input
           id="name"
           value={state["name"]}
           onChange={handlechange}
           placeholder="Add the Name of your place"
+          label="Name"
         />
-      </div>
-
-      <div
-        className={`place-form`}
-      >
+        <label>Place</label>
         <input
           id="place"
           value={state["place"]}
           onChange={handlechange}
           placeholder="Add additional information about your place"
         />
-      </div>
-
-      <div
-        className={`place-form`}
-      >
-        
+        <label>Location</label>
         <input
           id="location"
           value={state["location"]}
           onChange={handlechange}
-          placeholder="Add the Location of your Place"        
+          placeholder="Add the Location of your Place"
         />
-      </div>
-      <Button type="submit" disabled={!Object.keys(state).every((key) => (state[key] != ""))}>
-        ADD PLACE
-      </Button>
+          <Button type="submit" disabled={!Object.keys(state).every((key) => (state[key] != ""))}>
+          ADD PLACE
+        </Button>
+        </div>
     </form>
   );
 };
